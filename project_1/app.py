43d91bd29a401e0c2df813e40bf130c0f4d8f517
ivekhov <ivekhov@gmail.com>
2021-01-29 21:52:23 +0300

    Update app.py in project 1

--- project_1/app.py
+++ project_1/app.py
@@ -8,5 +8,16 @@ def main():
     return render_template('index.html')
 
 
-app.debug = True
-app.run('0.0.0.0', 5000)
+@app.route('/departures/<departure>/')
+def show_deparutes(departure):
+    return render_template('departure.html')
+
+
+@app.route('/tours/<id>/')
+def show_tours(id):
+    return render_template('tour.html')
+
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run('0.0.0.0', 5000)
